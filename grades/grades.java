f81d39655ab308f151c2cd032887fae1aa51692c
opas350 <opas350@gmail.com>
2016-07-26 20:16:01 -0500

    Add the correct file

--- grades/grades.java
+++ grades/grades.java
@@ -1,29 +1,45 @@
+package grades2;
 
-public class grades
+import java.util.Scanner;
+
+/**
+ * CourseGrade determines the final grade which is computed as
+ * the weighted sum of the grades obtained in exam, lab and homework
+ */
+public class grade
 {
-	public static void main(String[] args)
-	{	
-		int examWeight = 70;
-		int labWeight = 20;
-		int hwWeight = 10;
-		double examScore, labScore, hwScore, finalgrade;
+    public static void main(String[] args)
+    {
+        int examWeight = 70;    // Percentage weight given to examination
+        int labWeight = 20;     // Percentage weight given to lab work
+        int hwWeight = 10;      // Percentage weight given to homework assignment
+        double examScore;       // Examination score obtained by student
+        double labScore;        // Lab score obtained by student
+        double hwScore;         // Homework score obtained by student
+        double finalGrade;      // Final grade obtained by student
+        
+        Scanner s = new Scanner(System.in);
 
-		IO.output("Enter your exam grade: ");
-		examScore = IO.inputDouble();
-		IO.output("Enter your lab grade: ");
-		labScore = IO.inputDouble();
-		IO.output("Enter your homework grade: ");
-		hwScore = IO.inputDouble();
-
-// Computer final grade as the weighted sum of scores
-
-		examScore= examScore * (examWeight /100.0);
-		labScore * (labWeight /100.0);
-		hwScore = hwScore * (hwWeight / 100.0);
-		finalgrade = examScore + labScore + hwScore;
-
-// Output the final grade
-
-		IO.outputln("Your final grade is " + finalgrade);
-}
-}	
+        // Ask student to input scores for exam, lab and nomework
+        System.out.println("Enter your exam grade");
+        examScore = s.nextDouble();
+        
+        System.out.println("Enter your lab grade");
+        labScore = s.nextDouble();
+  
+        System.out.println("Enter your homework grade");
+        hwScore = s.nextDouble();
+     
+     
+        // Computer final grade as the weighted sum of exam, lab and homework scores
+	    examScore = examScore * (examWeight / 100.0);
+        labScore = labScore * (labWeight / 100.0);
+        hwScore = hwScore * (hwWeight / 100.0);
+        finalGrade = examScore + labScore + hwScore;
+        
+        // Output the final grade
+        System.out.println("Your final grade is "+finalGrade);
+      
+        
+    }
+}        
