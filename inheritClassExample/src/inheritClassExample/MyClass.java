21676216e8390de286185ae81f3a5aef1f986529
opas350 <opas350@gmail.com>
2016-07-29 00:58:14 -0500

    Add Inherit Class Example

---
+++ inheritClassExample/src/inheritClassExample/MyClass.java
@@ -0,0 +1,21 @@
+package inheritClassExample;
+
+class Animal {
+	protected int legs;
+	public void eat() {
+		System.out.println("Animal eats");
+	}
+}
+
+class Dog extends Animal {
+	Dog() {
+		legs = 4;
+	}
+}
+public class MyClass {
+	public static void main(String[]  args){
+		Dog d = new Dog( );
+				d.eat();
+	}
+
+}
