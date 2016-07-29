cb1d954d1668f740b35a435f016e332678b309d0
opas350 <opas350@gmail.com>
2016-07-29 01:48:37 -0500

    Add Abstraction Simple Example

---
+++ AbstractionSimpleEx/src/Program.java
@@ -0,0 +1,17 @@
+abstract class Animal {
+	int legs = 0;
+	abstract void makeSound();
+}
+
+class Cat extends Animal {
+	public void makeSound() {
+		System.out.println("Meow");
+	}
+}
+
+public class Program {
+	public static void main(String[] args) {
+		Cat c = new Cat();
+		c.makeSound();
+	}
+}
