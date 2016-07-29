9525eef86b3e2128878ca0e93e9d2fac7c4447a3
opas350 <opas350@gmail.com>
2016-07-29 01:28:22 -0500

    Add Method Overloading

---
+++ MethodOverloadingEx/src/MethodOverEx.java
@@ -0,0 +1,28 @@
+
+public class MethodOverEx {
+	static double max(double a, double b) {
+		if(a > b) {
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
+	
+	static int max(int a, int b) {
+		if(a > b){
+			return a;
+		}
+		else {
+			return b;
+		}
+	} 
+
+
+	public static void main(String[] args) {
+
+		System.out.println(max(8, 17));
+		System.out.println(max(3.14, 7.68));
+	}
+
+}
