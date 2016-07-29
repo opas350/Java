f6ddd91577ca91988ca8108f980e7c8947601846
opas350 <opas350@gmail.com>
2016-07-29 03:46:25 -0500

    Add Interface example

---
+++ InterfaceExample1/src/Interfaceex1.java
@@ -0,0 +1,21 @@
+interface Animal{
+	public void eat();
+	public void makeSound();
+}
+
+class Cat implements Animal {
+	public void makeSound() {
+		System.out.println("Meow");
+	}
+	public void eat() {
+		System.out.println("omnomnom");
+	}
+}
+
+public class Interfaceex1 {
+	public static void main(String[] args) {
+		Cat c = new Cat();
+		c.eat();
+		c.makeSound();
+	}
+}
